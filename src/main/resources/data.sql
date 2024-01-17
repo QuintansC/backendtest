e626eea75fa646e57b3db959ccffdf41ae11fcb3
QuintansC <gustavosantos.2095@gmail.com>
2024-01-17 06:57:21 -0300

    feat: estrutura da criptografia de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_user(nome, email, senha) VALUES ( 'quintansc', 'quintansc@gmail.com', '1234561');
-INSERT INTO tb_user(nome, email, senha) VALUES ( 'gustavo', 'gustavo@gmail.com', '1234561');
+INSERT INTO tb_user(nome, email, senha) VALUES ( 'quintansc', 'quintansc@gmail.com', '$2a$10$Vo.35/hJB8skj/UwKum61ev0.pbCcFsmNxw4znRifBWTptLauNhva');
+INSERT INTO tb_user(nome, email, senha) VALUES ( 'gustavo', 'gustavo@gmail.com', '$2a$10$Vo.35/hJB8skj/UwKum61ev0.pbCcFsmNxw4znRifBWTptLauNhva');
